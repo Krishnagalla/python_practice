eb7dbc7b08c84af457d190f18ea2dbdeb9e69580
Kri2 <k@g.com>
2021-11-15 23:37:01 +0530

    U

--- Git_Commit.py
+++ Git_Commit.py
@@ -28,27 +28,26 @@ print ("New files :"+ " " + ', '.join(add_list))
 modified_join = ', '.join(modified_list)
 add_join = ', '.join(add_list)
 username = sp.getoutput('git config user.name')
-print (username)
 
-# commit_ids = dict()
-# if  modified_join or add_join :
-# 	user_ack = input('The above mentioned files will be commited, Please confirm (Y/N):')
-# 	if user_ack.upper() == 'Y':
-# 		if add_join:
-# 			add = sp.getoutput(f'git add -A')
-# 			print(add)
-# 		if modified_join:
-# 			comment = input('Please provide a commit comment :')
-# 		if comment:
-# 			try:
-# 				comit = sp.getoutput(f'git commit -m {comment}')
-# 				print(f"commit {comit}")
-# 			except os.error as e:
-# 				print(f"unable to comment {e}")
-# 	else:
-# 		exit(0)
-# print ('Please take a note of list commit ids above')
-# print (modified_list)
-# print (add_list)
+commit_ids = dict()
+if  modified_join or add_join :
+	user_ack = input('The above mentioned files will be commited, Please confirm (Y/N):')
+	if user_ack.upper() == 'Y':
+		if add_join:
+			add = sp.getoutput(f'git add -A')
+			print(add)
+		if modified_join:
+			comment = input('Please provide a commit comment :')
+		if comment:
+			try:
+				comit = sp.getoutput(f'git commit -a -m {comment}')#git commit -a --author='Kri2 <>' -m 'Testing username commit'
+				print(f"commit {comit}")
+			except os.error as e:
+				print(f"unable to comment {e}")
+	else:
+		exit(0)
+print ('Please take a note of commit ids above')
+print (modified_list)
+print (add_list)
 #else:
 #    print('Please reenter the directory full path')
